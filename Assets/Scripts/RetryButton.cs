b3807fc48890c16c9df94930b993dccefcca6d55
esanche06 <ethands@uci.edu>
2017-11-05 00:02:32 -0700

    Added retry
    Two buttons show up when the user fails: Home and Retry. Home does
    nothing, but clicking retry reloads the scene.

---
+++ Assets/Scripts/RetryButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryButton : MonoBehaviour {
+
+	public void OnClick() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name); //.buildindex?
+	}
+}
---
+++ Assets/Scripts/ShowButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowButtons : MonoBehaviour {
+	private PlayerEclipseInput sunScript;
+
+	void Start () {
+		sunScript = GameObject.Find ("Sun").GetComponent<PlayerEclipseInput>();
+	}
+	
+	void FixedUpdate () {
+		if (sunScript.failure) {
+			gameObject.GetComponent<CanvasGroup> ().alpha = 1;
+		}
+	}
+}
