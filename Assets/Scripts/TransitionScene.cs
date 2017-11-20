2c5f16f18dc4c04fb2e9a6a8a14e852eb56fb1d2
Eleanor <eleanor@delux.com>
2017-11-19 23:34:50 -0800

    clean up transitionscene script

--- Assets/Scripts/TransitionScene.cs
+++ Assets/Scripts/TransitionScene.cs
@@ -5,14 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class TransitionScene : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
-
 	public void ChangeScene (string sceneName) {
 		Application.LoadLevel (sceneName);
 
