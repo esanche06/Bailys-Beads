bbc60cc3e34a43fc8bde99fb7ecb9f0d13283b47
esanche06 <ethands@uci.edu>
2017-11-20 21:37:07 -0800

    Rearranged Instructions Screen

--- Assets/Scripts/SolarFlare.cs
+++ Assets/Scripts/SolarFlare.cs
@@ -16,15 +16,18 @@ public class SolarFlare : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
+		if(!flaring)
+			timeTillNextFlare -= Time.deltaTime;
+		
 		if (timeSinceFlare > 5f) {
 			timeSinceFlare = 0f;
 			flaring = false;
-			timeTillNextFlare = Random.Range (10f, 15f);
 		}
 
-		if (Time.time % timeTillNextFlare == 0 && Time.time != 0) {
+		if (timeTillNextFlare <= 0 && Time.time != 0) {
 			flaring = true;
 			anim.Play ("SunFlare");
+			timeTillNextFlare = Random.Range (10f, 15f);
 		}
 
 		if (flaring)
