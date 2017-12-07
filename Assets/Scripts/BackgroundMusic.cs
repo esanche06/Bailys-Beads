5e16fb7dbe09011f04bf8837e6bdbea8e8f31153
phucmp <phucmp@users.noreply.github.com>
2017-12-06 17:05:29 -0800

    Audio Update
    Created a new GameObject (MusicSpeedController). Added Bonus  sound effect. Update Background music with speed up and slow down. Updated Failure sound

--- Assets/Scripts/BackgroundMusic.cs
+++ Assets/Scripts/BackgroundMusic.cs
@@ -6,15 +6,27 @@ public class BackgroundMusic : MonoBehaviour {
 	public AudioSource bgMusicSource;
 	public AudioClip bgMusicClip;
 
+	private AudioController audioController;
+
+
 	void Start () {
 		// Start Playing Background Music for Game
 		bgMusicSource = GetComponent<AudioSource> ();
 		bgMusicSource.clip = bgMusicClip;
 		bgMusicSource.Play ();
+
+		// Solar Flare Variable set
+		audioController = GameObject.Find ("MusicSpeedControl").GetComponent<AudioController> (); 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+	void FixedUpdate () {
+		// Control when to play and pause background music 
+		// Interacts with AudioController
+		if (audioController.isSoundOn) {
+			bgMusicSource.Pause ();
+		} else {
+			bgMusicSource.UnPause ();
+		}
 	}
 }
