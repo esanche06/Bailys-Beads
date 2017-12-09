ca097b17fb0d886deeff5290cffe9e187cc1e1cd
Eleanor <eleanor@delux.com>
2017-12-08 17:04:21 -0800

    Add aliens (spawner needs to be adjusted) (bonus to come later)

---
+++ Assets/Scripts/AlienController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienController : MonoBehaviour {
+	private GameObject target;
+	private Vector3 targetPosition, mousePosition;
+	private Vector2 targetDirection, startPos, endPos, direction, stop;
+	private float touchTimeStart, touchTimeFinish, timeInterval, mousePosX, mousePosY;
+
+	[Range (0.05f, 1f)]
+	public float throwForce = 0.3f;
+
+	void Start () {
+			if (transform.position.x < 0) {
+			target = GameObject.Find ("RightAlienTarget");
+			targetPosition = target.GetComponent<Transform> ().position - transform.position;
+			targetDirection = new Vector2 (targetPosition.x, targetPosition.y);
+			stop = new Vector2 (0, 0);
+
+			GetComponent<Rigidbody2D> ().AddForce (targetDirection * 10);
+		}
+		else {
+			target = GameObject.Find ("LeftAlienTarget");
+			targetPosition = target.GetComponent<Transform> ().position - transform.position;
+			targetDirection = new Vector2 (targetPosition.x, targetPosition.y);
+			stop = new Vector2 (0, 0);
+
+			GetComponent<Rigidbody2D> ().AddForce (targetDirection * 10);
+		}
+
+	}
+		
+
+
+}
