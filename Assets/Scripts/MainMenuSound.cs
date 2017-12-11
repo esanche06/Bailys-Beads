a03a3bfd15c650fdf2225d17d609b2b34020960d
phucmp <phucmp@users.noreply.github.com>
2017-12-10 23:31:57 -0800

    Background Music added to Main Menu

---
+++ Assets/Scripts/MainMenuSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSound : MonoBehaviour {
+	public AudioSource bgMusicSource;
+	public AudioClip bgMusicClip;
+
+	// Use this for initialization
+	void Start () {
+		// Start Playing Background Music for Game
+		bgMusicSource = GetComponent<AudioSource> ();
+		bgMusicSource.clip = bgMusicClip;
+		bgMusicSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
